4f77e2a911d57b6abb1d1f6cb37c21ca13c03a35
vimalprakash <vimalprakash332@gmail.com>
2023-09-04 17:47:08 +0530

    modified

--- src/component/Admin.js
+++ src/component/Admin.js
@@ -1,154 +1,13 @@
 import { useEffect, useState } from "react";
 import Gun from "gun";
 import jsonData from './../datagh.json';
-const users_variable = {
-    "001": {
-      Name: "Emily Johnson",
-      Institution: "St. Teresa's College, Ernakulam",
-      Email: "emily.johnson@example.com",
-      Phone: "123-456-7890",
-      Verified: false
-    },
-    "002": {
-      Name: "Michael Smith",
-      Institution: "St. Xavier's College, Thiruvananthapuram",
-      Email: "michael.smith@example.com",
-      Phone: "987-654-3210",
-      Verified: false
-    },
-    "003": {
-      Name: "Sophia Williams",
-      Institution: "Sacred Heart College, Kochi",
-      Email: "sophia.williams@example.com",
-      Phone: "456-789-0123",
-      Verified: false
-    },
-    "004": {
-      Name: "Ethan Brown",
-      Institution: "Maharaja's College, Kochi",
-      Email: "ethan.brown@example.com",
-      Phone: "789-012-3456",
-      Verified: false
-    },
-    "005": {
-      Name: "Olivia Davis",
-      Institution: "Government Brennen College, Thalassery",
-      Email: "olivia.davis@example.com",
-      Phone: "234-567-8901",
-      Verified: false
-    },
-    "006": {
-      Name: "Daniel Jones",
-      Institution: "Christ College, Irinjalakuda",
-      Email: "daniel.jones@example.com",
-      Phone: "890-123-4567",
-      Verified: false
-    },
-    "007": {
-      Name: "Ava Martinez",
-      Institution: "St. Berchmans College, Changanassery",
-      Email: "ava.martinez@example.com",
-      Phone: "567-890-1234",
-      Verified: false
-    },
-    "008": {
-      Name: "Liam Anderson",
-      Institution: "Farook College, Kozhikode",
-      Email: "liam.anderson@example.com",
-      Phone: "901-234-5678",
-      Verified: false
-    },
-    "009": {
-      Name: "Isabella Thomas",
-      Institution: "St. Joseph's College, Devagiri",
-      Email: "isabella.thomas@example.com",
-      Phone: "123-456-7890",
-      Verified: false
-    },
-    "010": {
-      Name: "Noah Wilson",
-      Institution: "Mar Ivanios College, Thiruvananthapuram",
-      Email: "noah.wilson@example.com",
-      Phone: "987-654-3210",
-      Verified: false
-    },
-    "011": {
-      Name: "Mia Taylor",
-      Institution: "Baselius College, Kottayam",
-      Email: "mia.taylor@example.com",
-      Phone: "456-789-0123",
-      Verified: false
-    },
-    "012": {
-      Name: "Lucas Martinez",
-      Institution: "Bishop Moore College, Mavelikara",
-      Email: "lucas.martinez@example.com",
-      Phone: "789-012-3456",
-      Verified: false
-    },
-    "013": {
-      Name: "Amelia White",
-      Institution: "Providence Women's College, Calicut",
-      Email: "amelia.white@example.com",
-      Phone: "234-567-8901",
-      Verified: false
-    },
-    "014": {
-      Name: "Henry Lee",
-      Institution: "St. Thomas College, Thrissur",
-      Email: "henry.lee@example.com",
-      Phone: "890-123-4567",
-      Verified: false
-    },
-    "015": {
-      Name: "Harper Johnson",
-      Institution: "Government College for Women, Thiruvananthapuram",
-      Email: "harper.johnson@example.com",
-      Phone: "567-890-1234",
-      Verified: false
-    },
-    "016": {
-      Name: "Aiden Kim",
-      Institution: "Malabar Christian College, Kozhikode",
-      Email: "aiden.kim@example.com",
-      Phone: "901-234-5678",
-      Verified: false
-    },
-    "017": {
-      Name: "Elizabeth Wang",
-      Institution: "Sree Narayana College, Kollam",
-      Email: "elizabeth.wang@example.com",
-      Phone: "123-456-7890",
-      Verified: false
-    },
-    "018": {
-      Name: "Benjamin Nguyen",
-      Institution: "St. Albert's College, Kochi",
-      Email: "benjamin.nguyen@example.com",
-      Phone: "987-654-3210",
-      Verified: false
-    },
-    "019": {
-      Name: "Chloe Garcia",
-      Institution: "Nirmala College, Muvattupuzha",
-      Email: "chloe.garcia@example.com",
-      Phone: "456-789-0123",
-      Verified: false
-    },
-    "020": {
-      Name: "Alexander Hernandez",
-      Institution: "College of Engineering Vadakara",
-      Email: "alexander.hernandez@example.com",
-      Phone: "789-012-3456",
-      Verified: false
-    }
-  };
+
 function Admin()
 {
   const [verified_count,updatecount] = useState(0)
-    const index="init123"
+    const index="init1234"
     const gun = Gun({
-        peers: ['http:192.168.102.163:5000/gun']
+        peers: ['http://localhost:5000/gun']
       })
   
   function putdata()
@@ -206,10 +65,9 @@ function Admin()
     
     function fetchdata(item , ind)
     {
-      var userdata ={};
+     
       setUser([])
       gun.get(index+'/'+item).once((node) =>{
-        userdata=node;
         setUser(data => data.concat({"Name":node.Name,"ID":item,"Institution":node.Institution,"Email":node.Email,"Phone":node.Phone,"Verified":node.Verified}));
       })    
     }
@@ -228,7 +86,7 @@ function Admin()
         gun.get(index+'/'+pad(i,3)).on((node) => { 
           
             if(node.Verified)
-            {
+            { 
               if (!(users===[]))
                 change_status(pad(i,3))
               
@@ -238,14 +96,7 @@ function Admin()
           
         })
       }
-    //   gun.get(index).on((node) => { // Is called whenever text is updated
-    //     console.log(node)
-    //   })
-    //   gun.get(index+'/'+"init").on((node) => { // Is called whenever text is update
-    //     getdata()
-    // })
-    //   gun.get(index+'/'+'001')
-      
+    
   
     }, [])
     const [users,setUser] = useState([])
@@ -306,7 +157,7 @@ function Admin()
                               <tbody>
                                 {users.map(da => 
                                 <tr>
-                                  <th scope="row">
+                                  <th >
                                   {da.ID}
                                   </th>
                                   <td>
